1cc32e20128ef9ef7603400606dfaa829282bc84
Juba Sarni <juba@localhost.localdomain>
2020-05-31 16:58:51 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,14 @@ use std::{env, process::Command};
 fn main() {
     let mut hashcat = String::from("");
     let args = env::args().skip(1);
+    let mut f;
 
     for arg in args {
         let entries =
-            walkdir::WalkDir::new(arg)
-                .into_iter()
-                .filter_map(Result::ok)
-                .filter(|e| !e.file_type().is_dir());
+            walkdir::WalkDir::new(arg).into_iter().filter_map(Result::ok).filter(|e| !e.file_type().is_dir());
 
         for entry in entries {
-            let f = String::from(entry.path().to_string_lossy());
+            f = String::from(entry.path().to_string_lossy());
             let mut cmd = Command::new("sh");
             cmd.arg("-c").arg(format!("md5sum \"{}\"", f));
             let output = String::from_utf8_lossy(&cmd.output().unwrap().stdout).to_string();
